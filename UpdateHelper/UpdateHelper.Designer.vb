f3fbad9e686b03789eeb0b8264809bd447fcaf0a
Balthild Ires <stingzz.go@gmail.com>
2015-07-26 22:04:57 +0800

    Update Helper 界面

--- UpdateHelper/UpdateHelper.Designer.vb
+++ UpdateHelper/UpdateHelper.Designer.vb
@@ -22,9 +22,58 @@ Partial Class UpdateHelper
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.UpdateProgress = New System.Windows.Forms.ProgressBar()
+        Me.LogList = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'UpdateProgress
+        '
+        Me.UpdateProgress.Location = New System.Drawing.Point(12, 267)
+        Me.UpdateProgress.Name = "UpdateProgress"
+        Me.UpdateProgress.Size = New System.Drawing.Size(450, 23)
+        Me.UpdateProgress.TabIndex = 0
+        Me.UpdateProgress.Value = 10
+        '
+        'LogList
+        '
+        Me.LogList.FormattingEnabled = True
+        Me.LogList.ItemHeight = 17
+        Me.LogList.Location = New System.Drawing.Point(12, 42)
+        Me.LogList.Name = "LogList"
+        Me.LogList.Size = New System.Drawing.Size(450, 208)
+        Me.LogList.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "进度："
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'UpdateHelper
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(474, 311)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LogList)
+        Me.Controls.Add(Me.UpdateProgress)
+        Me.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "UpdateHelper"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "正在更新MoeCraft更新器"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents UpdateProgress As ProgressBar
+    Friend WithEvents LogList As ListBox
+    Friend WithEvents Label1 As Label
 End Class
--- UpdateHelper/UpdateHelper.vb
+++ UpdateHelper/UpdateHelper.vb
@@ -1,3 +1,25 @@
-﻿Public Class UpdateHelper
+﻿Imports System.IO
 
+Public Class UpdateHelper
+    Private Sub Log(Info As String)
+        LogList.Items.Add(Info)
+        LogList.SelectedItem = Info
+    End Sub
+
+    Private Sub UpdateHelper_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Log("更新器已启动")
+
+        Dim MyPath As String = IIf(Strings.Right(My.Application.Info.DirectoryPath, 1) = "\", My.Application.Info.DirectoryPath, My.Application.Info.DirectoryPath & "\")
+        Dim Args() As String = System.Environment.GetCommandLineArgs
+
+        If UBound(Args) <= 0 Then
+            Log("启动参数错误，更新中止")
+            Application.Exit()
+        End If
+
+        Dim FileName As String = Args(0)
+        If File.Exists(MyPath & FileName) = False Then
+
+        End If
+    End Sub
 End Class
